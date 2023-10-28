3111d4dab871558b3f76623b38646daabcee57e0
MikeMaroni9 <k.vaitka@gmail.com>
2023-10-28 14:34:26 +0000

    test for the postdetail view added, make sure that the user can retrieve a valid posts, can't retrieve invalid one. That they can update their own posts and that they CAN'T update others

--- posts/tests.py
+++ posts/tests.py
@@ -26,3 +26,35 @@ class PostListViewTests(APITestCase):
     def test_user_not_logged_in_cant_create_post(self):
         response = self.client.post('/posts/', {'title': 'a title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        cheko = User.objects.create_user(username='cheko', password='qweqwe123123')
+        joebiden = User.objects.create_user(username='joebiden', password='qweqwe123123')
+        Post.objects.create(
+            owner=cheko, title='a title', content='cheko content'
+        )
+        Post.objects.create(
+            owner=joebiden, title='another title', content='joebiden content'
+        )
+
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['title'], 'a title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_post_using_invalid_id(self):
+        response = self.client.get('/posts/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_post(self):
+        self.client.login(username='cheko', password='qweqwe123123')
+        response = self.client.put('/posts/1/', {'title': 'a new title'})
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(post.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cant_update_another_users_post(self):
+        self.client.login(username='cheko', password='qweqwe123123')
+        response = self.client.put('/posts/2/', {'title': 'a new title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
