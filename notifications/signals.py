c9e8d52a330f4b5ca93653ef71a35dab118c3e62
MikeMaroni9 <k.vaitka@gmail.com>
2024-02-04 18:50:49 +0000

    reverting the changes

--- notifications/signals.py
+++ notifications/signals.py
@@ -1,15 +1,18 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from likes.models import Like
+from likes.models import Like 
 from notifications.models import Notification
 from django.contrib.auth.models import User
+from django.core.signals import Signal
 
 @receiver(post_save, sender=Like)
 def create_like_notification(sender, instance, created, **kwargs):
+    print("Signal received!")
     if created:
+        print(f"Like created by {instance.owner} on post {instance.post}")
         Notification.objects.create(
             recipient=instance.post.owner,
-            sender_profile_id=instance.owner.profile.id,
+            sender=instance.owner,
             post=instance.post,
             notification_type='like'
         )
