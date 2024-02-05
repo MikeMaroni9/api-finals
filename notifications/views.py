4c73c1fec0ad16635df2828ead6e3c0ba0c6e348
MikeMaroni9 <k.vaitka@gmail.com>
2024-02-05 04:31:07 +0000

    notifications views adjusted

--- notifications/views.py
+++ notifications/views.py
@@ -1,11 +1,20 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Notification
 from .serializers import NotificationSerializer
 
 class NotificationListCreateView(generics.ListCreateAPIView):
-    queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Notification.objects.filter(recipient=user)
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user, recipient=self.request.user)
 
 class NotificationDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
+    permission_classes = [IsAuthenticated]
